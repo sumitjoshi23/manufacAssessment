5c5974f652f44edda8c268464622d5e23943029d
Sumit <joshiz.sumit@gmail.com>
2023-03-30 01:46:16 +0530

    This is initial commit

---
+++ src/components/BarChart.js
@@ -0,0 +1,79 @@
+import ReactEcharts from "echarts-for-react";
+import wineData from "../utils/Wine-Data.json";
+
+// Map to store alcohol categories and required wine data
+const traveresedWineData = new Map();
+const alcoholCategories = [];
+const malicAcidAvg = [];
+
+for (let i = 0; i < wineData.length; i++) {
+  const currentWine = wineData[i];
+
+  // If alcohol category of the current wine is not present in traversed wine data map,
+  // add it with it's malic acid, alcohol category and assign a new count property to value 1
+  if (!traveresedWineData.has(currentWine.Alcohol)) {
+    const updatedAlcoholData = {
+      count: 1,
+      malicAcidSum: currentWine["Malic Acid"],
+    };
+
+    // Adding key as alcohol category and object having count and malic acid sum as value to the map
+    traveresedWineData.set(currentWine.Alcohol, updatedAlcoholData);
+  } else {
+    // If the alcohol category of current wine was already present inside traversed wine data,
+    // increase the count of category by 1, also update the malic acid sum.
+    const { count, malicAcidSum } = traveresedWineData.get(currentWine.Alcohol);
+    const sum = currentWine["Malic Acid"] + malicAcidSum;
+    const updatedAlcoholData = {
+      count: count + 1,
+      malicAcidSum: sum,
+    };
+
+    // Adding key as alcohol category and object having count and malic acid sum as value to the map
+    traveresedWineData.set(currentWine.Alcohol, updatedAlcoholData);
+  }
+}
+
+traveresedWineData.forEach((value, key) => {
+  alcoholCategories.push(key); // Adds all keys i.e. alcohol categories to the alcohol categories array
+  malicAcidAvg.push(value.malicAcidSum / value.count); // Adds average of malic acid to the malic acid average array
+});
+
+// Bar chart component to be displayed
+const BarChart = () => {
+  const option = {
+    title: {
+      text: "Wine Data Bar Chart",
+      left: "center",
+      top: 20,
+    },
+
+    // Added tooltip in option object to have a tooltip on chart data points
+    tooltip: {},
+    xAxis: {
+      name: "Alcohol",
+      type: "category",
+      data: alcoholCategories,
+      nameLocation: "middle",
+      nameGap: 30,
+    },
+
+    yAxis: {
+      type: "value",
+      name: "Malic Acid Average",
+      nameLocation: "middle",
+      nameGap: 40,
+    },
+
+    series: [
+      {
+        name: "Alcohol Category vs Malic Acid Average",
+        type: "bar",
+        data: malicAcidAvg,
+      },
+    ],
+  };
+  return <ReactEcharts option={option} />;
+};
+
+export default BarChart;
---
+++ src/components/ScatterChart.js
@@ -0,0 +1,46 @@
+import ReactEcharts from "echarts-for-react";
+import wineData from "../utils/Wine-Data.json";
+
+// Getting the array of color intensities of the wines and sorting them in ascending order
+const colorIntensity = wineData
+  .map((wine) => wine["Color intensity"])
+  .sort((a, b) => a - b);
+
+// Gets the array of hue of wines
+const hue = wineData.map((wine) => wine["Hue"]);
+
+// Scatter chart component to be displayed
+const ScatterChart = () => {
+  const option = {
+    title: {
+      text: "Wine Data Scatter Chart",
+      left: "center",
+      top: 20,
+    },
+
+    // Added tooltip in option object to have a tooltip on chart data points
+    tooltip: {},
+    xAxis: {
+      name: "Color intensity",
+      data: colorIntensity,
+      nameGap: 30,
+      nameLocation: "middle",
+    },
+    yAxis: {
+      type: "value",
+      name: "Hue",
+      nameLocation: "middle",
+      nameGap: 40,
+    },
+    series: [
+      {
+        name: "Color Intensity vs Hue",
+        type: "scatter",
+        data: hue,
+      },
+    ],
+  };
+  return <ReactEcharts option={option} />;
+};
+
+export default ScatterChart;
